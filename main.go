5ef3bbddd1b1da86ca6542835a988a130f07f295
bloodycrayfish <m1992steen@gmail.com>
2024-07-13 14:28:28 +0200

    testing auto formatting in nvim

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 )
 
 func main() {
-    fmt.Print("Press enter to close...")
-    bufio.NewReader(os.Stdin).ReadBytes('\n')
+	fmt.Print("Press enter to close...")
+	bufio.NewReader(os.Stdin).ReadBytes('\n')
+	fmt.Print("test")
+	if "test" == "test" {
+		fmt.Print("Test")
+	}
 }
